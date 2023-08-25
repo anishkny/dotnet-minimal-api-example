0af4694fe0deb7de5977474f766e71780bf0d60f
Anish Karandikar <anishkny@gmail.com>
2023-08-25 11:00:02 -0700

    test: Add Postman basic test (#1)

--- Program.cs
+++ Program.cs
@@ -3,4 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// Handle HEAD requests (needed for start-server-and-test)
+app.MapMethods("/", new[] { "HEAD" }, () => "");
+
 app.Run();
