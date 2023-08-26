e0b3afa6e28ea8acdca4efe0e9bcc5f17db2cb4d
Anish Karandikar <anishkny@gmail.com>
2023-08-26 11:40:52 -0700

    feat: Add route handlers (#3) (#7)
    * feat: Add route handlers (#3)
    * feat: Add route handlers
    * fix: Specify nullable context
    * test: Test deleting unknown item
    
    * fix: Ignore secret accessors codecov

--- src/Todo.cs
+++ src/Todo.cs
@@ -1,6 +1,22 @@
 #nullable enable
 
+using System.Diagnostics.CodeAnalysis;
+
 public class Todo
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public bool IsComplete { get; set; }
+
+  [ExcludeFromCodeCoverage]
+  public string? Secret { get; set; }
+
+  // Convert to DTO
+  public TodoDTO ToDTO() => new TodoDTO
+  { Id = this.Id, Name = this.Name, IsComplete = this.IsComplete };
+}
+
+public class TodoDTO
 {
   public int Id { get; set; }
   public string? Name { get; set; }
