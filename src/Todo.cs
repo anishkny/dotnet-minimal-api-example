25f0afb27db5497f166f1e3318123e0bcec3ecf6
Anish Karandikar <anishkny@gmail.com>
2023-08-26 11:36:32 -0700

    fix: Ignore secret accessors codecov

--- src/Todo.cs
+++ src/Todo.cs
@@ -1,10 +1,14 @@
 #nullable enable
 
+using System.Diagnostics.CodeAnalysis;
+
 public class Todo
 {
   public int Id { get; set; }
   public string? Name { get; set; }
   public bool IsComplete { get; set; }
+
+  [ExcludeFromCodeCoverage]
   public string? Secret { get; set; }
 
   // Convert to DTO
