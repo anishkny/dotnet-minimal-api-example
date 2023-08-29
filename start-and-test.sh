f938e48c05c548dce42e5c688064fd67313050f7
Anish Karandikar <anishkny@gmail.com>
2023-08-28 19:08:14 -0700

    chore(build): Fail on warnings Closes #4

--- start-and-test.sh
+++ start-and-test.sh
@@ -2,19 +2,9 @@
 set -euxo pipefail
 
 PORT=5000
-
-# Start the server
+npm run build
 npm start &
-sleep 2
-
-# Wait for the server to start
 npx wait-on http://localhost:$PORT
-
-# Run the tests
 npm test
-
-# Stop the server
 npm run stop
-
-# Generate code coverage report
 npm run coverage-report
